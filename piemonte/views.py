3d6bdccf989589ea64a93dade678aa0a8eb4de6a
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-5.local>
2023-08-02 11:34:42 -0300

    Templates commit

--- piemonte/views.py
+++ piemonte/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Index</h1>')
+    return render(request, 'piemonte/index.html')
