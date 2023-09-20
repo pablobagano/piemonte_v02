696cede6c2d52c898d904ad5aaf3f31f9cd3af2c
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-6.local>
2023-09-20 14:52:51 -0300

    Allowed hosts

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = str(os.getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1')).split(',')
 
 
 # Application definition
