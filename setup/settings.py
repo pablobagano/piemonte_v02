6cf3d1db84b58508fdac375f76b1d5b23a51d250
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-6.local>
2023-09-25 09:14:18 -0300

    Messages fixed

--- setup/settings.py
+++ setup/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Messages 
-# MESSAGE_TAGS = { 
-#     messages.ERROR = 'fail',
-#     messages.SUCCESS = 'success'
-# }
+MESSAGE_TAGS = { 
+    messages.ERROR : 'fail',
+    messages.SUCCESS : 'success'
+}
