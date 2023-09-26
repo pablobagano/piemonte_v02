f97fc33c27d6bc82d3c1d268a5bc4cd7eba6fcb8
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-6.local>
2023-09-26 15:11:26 -0300

    Debug = True. Tests

--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = str(os.getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1')).split(',')
 
